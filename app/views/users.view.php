598baac29814070523aeadde0fb5e6a5b1c70086
Igor-Fam <igor.mfam@gmail.com>
2020-06-18 15:46:25 -0300

    Testando imagens na database

--- app/views/users.view.php
+++ app/views/users.view.php
@@ -3,7 +3,15 @@
     <h1>All Users</h1>
 
 <?php foreach ($users as $user) : ?>
-    <li><?= $user->username; ?></li>
+ <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="<?= $user->email; ?>" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
 <?php endforeach; ?>
 
 <?php require('partials/footer.php'); ?>
